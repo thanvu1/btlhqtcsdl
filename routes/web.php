cb94b28879fb75994c41e085e5c8231d3cb2e39d
thanvu1 <hoangthanvu11@gmail.com>
2025-01-03 17:10:16 +0700

    Them route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,31 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminSetupController;
+use App\Http\Controllers\LoaiPhongController;
+use App\Http\Controllers\PhongController;
+use App\Http\Controllers\KhachHangController;
+use App\Http\Controllers\BoPhanController;
+use App\Http\Controllers\NhanVienController;
+use App\Http\Controllers\PhieuThueController;
+use App\Http\Controllers\HoaDonThanhToanController;
+use App\Http\Controllers\DichVuController;
+use App\Http\Controllers\PhieuDichVuController;
+use App\Http\Controllers\ChiTietPhieuDichVuController;
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('loaiphong', LoaiPhongController::class);
+    Route::resource('phong', PhongController::class);
+    Route::resource('khachhang', KhachHangController::class);
+    Route::resource('bophan', BoPhanController::class);
+    Route::resource('nhanvien', NhanVienController::class);
+    Route::resource('phieuthue', PhieuThueController::class);
+    Route::resource('hoadon', HoaDonThanhToanController::class);
+    Route::resource('dichvu', DichVuController::class);
+    Route::resource('phieudichvu', PhieuDichVuController::class);
+    Route::resource('chitietphieu', ChiTietPhieuDichVuController::class);
+});
+
+
 Route::get('create-admin', [AdminSetupController::class, 'createAdmin']);
 
 
